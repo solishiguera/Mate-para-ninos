1bbe9510f2b5d2650693173e9f2aaa890a265d81
Diego <A00827847@itesm.mx>
2022-08-29 16:48:29 -0500

    m2-5 is done

--- assets/js/scripts/modulo2/modulo2-5.js
+++ assets/js/scripts/modulo2/modulo2-5.js
@@ -2,24 +2,25 @@ $(document).ready(function() {
   $("#basicform").validate();
 });
 
+$("#basicform").submit(function(e) {
+  e.preventDefault();
+});
+
 var labelNames = ['p1', 'p2', 'p3', 'p3-1', 'p3-2','p3-3','p3-4', 'p4-4','p4','p4-1','p4-2','p4-3',]
-var labelAnsw = ['p1', 'p4', 'p3', 'p3-1', 'p3-2','p3-3','p3-4', 'p4-4','p4','p4-1','p4-2','p4-3',]
+var labelAnsw = ['54.858', '923.87', '7', '8', '7','3','9', '2','9','7','8','3']
 
 function correct() { 
-  if(document.basicform.p1.value == "" ||  document.basicform.currElement.value != labelAnsw[i]) {
-    document.getElementById(p1).style.backgroundColor = 'red'
-  } else { 
-    document.getElementById(p1).style.backgroundColor = 'green'
-  }  
-  /**
   for(let i = 0; i < labelNames.length; i++) { 
     var currElement = labelNames[i]
-    console.log(currElement)
-    console.log(document.basicform.currElement.value)
-    if(document.basicform.currElement.value == "" ||  document.basicform.currElement.value != labelAnsw[i]) {
-      document.getElementById(currElement).style.backgroundColor = 'red'
+    var myValue = document.getElementById(currElement).value
+    if(myValue == "" ||  !myValue.includes(labelAnsw[i])) {
+      document.getElementById(currElement).style.backgroundColor = '#fbdfde'
+      document.getElementById(currElement).style.color = 'red'
     } else { 
-      document.getElementById(currElement).style.backgroundColor = 'green'
+      document.getElementById(currElement).style.backgroundColor = '#dcecd8'
+      document.getElementById(currElement).style.color = 'green'
     }
-  } */
+  }
 }
+
+document.getElementById("submitBtn").addEventListener("click", correct, false);
