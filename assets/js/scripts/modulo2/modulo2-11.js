e3f5aa5c5bd86acd8361c5e961b597f2ee77e39e
Diego <A00827847@itesm.mx>
2022-08-31 14:40:01 -0500

    reestructura

--- assets/js/scripts/modulo2/modulo2-11.js
+++ assets/js/scripts/modulo2/modulo2-11.js
@@ -1,17 +1,52 @@
-function isAnswer(curr, correct) { 
-  if(curr == correct) {
-    document.getElementById(correct).style.backgroundColor = '#dcecd8'
-  } else {
-    document.getElementById(curr).style.backgroundColor = '#fbdfde'
-  }
-  
-  document.getElementById(curr).classList.add("answered");
+/* draggable element */
+//const item = document.querySelector('.item');
+const items = document.getElementsByClassName('.items')
+
+item.addEventListener('dragstart', dragStart);
+
+function dragStart(e) {
+    e.dataTransfer.setData('text/plain', e.target.id);
+    setTimeout(() => {
+        e.target.classList.add('hide');
+    }, 0);
 }
 
-function resetColors() { 
-  var btns = document.getElementsByClassName('cool-btn')
-  for(let i = 0; i < btns.length; i++) {
-    btns[i].style.backgroundColor = '#93bed8'
-    btns[i].classList.remove("answered")
-  }
+
+/* drop targets */
+const boxes = document.querySelectorAll('.box');
+
+boxes.forEach(box => {
+    box.addEventListener('dragenter', dragEnter)
+    box.addEventListener('dragover', dragOver);
+    box.addEventListener('dragleave', dragLeave);
+    box.addEventListener('drop', drop);
+});
+
+
+function dragEnter(e) {
+    e.preventDefault();
+    e.target.classList.add('drag-over');
+}
+
+function dragOver(e) {
+    e.preventDefault();
+    e.target.classList.add('drag-over');
+}
+
+function dragLeave(e) {
+    e.target.classList.remove('drag-over');
+}
+
+function drop(e) {
+    e.target.classList.remove('drag-over');
+
+    // get the draggable element
+    const id = e.dataTransfer.getData('text/plain');
+    const draggable = document.getElementById(id);
+
+    // add it to the drop target
+    e.target.appendChild(draggable);
+
+    // display the draggable element
+    draggable.classList.remove('hide');
 }
---
+++ assets/js/scripts/modulo2/modulo2-17.js
@@ -0,0 +1,17 @@
+function isAnswer(curr, correct) { 
+  if(curr == correct) {
+    document.getElementById(correct).style.backgroundColor = '#dcecd8'
+  } else {
+    document.getElementById(curr).style.backgroundColor = '#fbdfde'
+  }
+  
+  document.getElementById(curr).classList.add("answered");
+}
+
+function resetColors() { 
+  var btns = document.getElementsByClassName('cool-btn')
+  for(let i = 0; i < btns.length; i++) {
+    btns[i].style.backgroundColor = '#93bed8'
+    btns[i].classList.remove("answered")
+  }
+}
