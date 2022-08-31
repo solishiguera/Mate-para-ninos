493ccdcbbe0738b2829c34fb9650b4ba4849786b
Diego <A00827847@itesm.mx>
2022-08-30 19:19:14 -0500

    working m2-8

--- assets/js/scripts/modulo2/modulo2-8.js
+++ assets/js/scripts/modulo2/modulo2-8.js
@@ -1,3 +1,14 @@
-function isAnswer(answ, id) { 
-  document.getElementById(id).style.backgroundColor = answ ? 'green' : 'red'
+function isAnswer(curr, correct) { 
+  if(curr == correct) {
+    document.getElementById(correct).style.backgroundColor = 'green'
+  } else {
+    document.getElementById(curr).style.backgroundColor = 'red'
+  }
+}
+
+function resetColors() { 
+  var btns = document.getElementsByClassName('cool-btn')
+  for(let i = 0; i < btns.length; i++) {
+    btns[i].style.backgroundColor = '#93bed8'
+  }
 }
