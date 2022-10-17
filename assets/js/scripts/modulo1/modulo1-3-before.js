d516ab1acd8e3905fdd622944d6d8dd62bbae8c8
Diego <A00827847@itesm.mx>
2022-10-17 16:29:29 -0500

    m1-3 done

--- assets/js/scripts/modulo1/modulo1-3-before.js
+++ assets/js/scripts/modulo1/modulo1-3-before.js
@@ -1,19 +1,20 @@
 $(document).ready(function() {
-  $("#basicform").validate();
+  $(".basicform").validate();
 });
 
-$("#basicform").submit(function(e) {
+$(".basicform").submit(function(e) {
   e.preventDefault();
 });
 
-var labelNames = ['p1', 'p2', 'p3', 'p4', 'p5', 'p6']
-var labelAnsw = ['+15', '-2', '+2', '+3', '+7', '-9']
+var labelNames = ['p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7', 'p8', 'p9', 'p10', 'p11', 'p12', 'p13','p14', 'p15', 'p16', 'p17', 'p18', 'p19', 'p20']
+var labelAnsw = ['+15', '-2', '+2', '+3', '+7', '-9', '64', '256', '1024', '36', '18', '9', 'restar15', '40', '25', '10', 'sumar8', '17', '25', '33']
 
 function correct() { 
   for(let i = 0; i < labelNames.length; i++) { 
     var currElement = labelNames[i]
     var myValue = document.getElementById(currElement).value
     myValue = myValue.replace(/\s/g, '');
+    myValue = myValue.toString().toLowerCase();
     if(myValue == "" ||  !myValue.includes(labelAnsw[i])) {
       document.getElementById(currElement).style.backgroundColor = '#fbdfde'
       document.getElementById(currElement).style.color = 'red'
