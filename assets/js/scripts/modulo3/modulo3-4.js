3f64e045020c268afcbb2ec42344be8249839188
Diego <A00827847@itesm.mx>
2022-10-18 23:37:19 -0500

    m3 work

---
+++ assets/js/scripts/modulo3/modulo3-4.js
@@ -0,0 +1,17 @@
+function isAnswer(curr, correct) { 
+  if(correct) {
+    document.getElementById(curr).style.backgroundColor = '#dcecd8'
+  } else {
+    document.getElementById(curr).style.backgroundColor = '#fbdfde'
+  }
+  
+  document.getElementById(curr).classList.add("answered");
+}
+
+function resetColors() { 
+  var btns = document.getElementsByClassName('cool-btn')
+  for(let i = 0; i < btns.length; i++) {
+    btns[i].style.backgroundColor = '#cd764d'
+    btns[i].classList.remove("answered")
+  }
+}
